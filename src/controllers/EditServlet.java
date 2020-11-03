15a7c599858979e97dc56fdaf540fa1d84344709
Yudai.Noto <eiyuvoxx1138@gmail.com>
2020-11-03 23:06:22 +0900

    Add UpdateServlet.java

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -38,7 +38,10 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-        request.setAttribute("task_id", t.getId());
+        request.setAttribute("task", t);
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("task_id", t.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
